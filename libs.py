25ef938d0d52633bb5c5abf20879403ec3e68b73
juheesvt <kangjuhee18@kookmin.ac.kr>
2021-10-04 18:42:13 +0900

    ipexclusion

--- libs.py
+++ libs.py
@@ -1,4 +1,7 @@
 import base64
+import json
+import urllib
+
 import requests
 
 import time
@@ -12,14 +15,16 @@ def getNow() -> int:
     now = time.time()
     return now
 
-def getSignature(path:str, method:str, API_SECRET:str) -> tuple:
+
+def getSignature(path: str, method: str, API_SECRET: str) -> tuple:
     time = int(getNow())
     message = '{}.{}.{}'.format(time, method, path)
     signature = base64.b64encode(hmac.new(API_SECRET.encode(), msg=message.encode(),
                                           digestmod=hashlib.sha256).digest())
     return signature, time
 
-def getCampagins(API_KEY:str, API_SECRET:str, CUSTOMER_ID:str) -> list:
+
+def getCampagins(API_KEY: str, API_SECRET: str, CUSTOMER_ID: str) -> list:
     PATH = "/ncc/campaigns"
 
     ses = requests.Session()
@@ -32,15 +37,56 @@ def getCampagins(API_KEY:str, API_SECRET:str, CUSTOMER_ID:str) -> list:
     })
     return res.json()
 
-def tests():
-    customer_id = ""
-    API_SECRET = ''
-    api_key = ''
 
-    campaigns = getCampagins(api_key,API_SECRET,customer_id)
+def getAdsGroups(API_KEY: str, API_SECRET: str, NCC_CAMPAIGN_Id: str, CUSTOMER_ID: str) -> list:
+    PATH = "/ncc/adgroups"
+    QUERY = {
+        'nccCampaignId': NCC_CAMPAIGN_Id
+    }
 
-    campaignId = campaigns[0]["nccCampaignId"]
-    print(campaignId)
+    ses = requests.Session()
+    signature, time = getSignature(PATH, "GET", API_SECRET)
+    res = ses.get(API_URL + PATH, headers={
+        "X-API-KEY": API_KEY,
+        "X-Signature": signature,
+        "X-Timestamp": str(time),
+        "X-CUSTOMER": CUSTOMER_ID,
+    }, params=QUERY)
+    return res.json()
 
 
-tests()
+def getAdsGroups(API_KEY: str, API_SECRET: str, CUSTOMER_ID: str) -> list:
+    pass
+
+
+def setIpExclusion(API_KEY: str, API_SECRET: str, CUSTOMER_ID: str, ip: str) -> list:
+    PATH = "/tool/ip-exclusions"
+    body = {
+        "filterIp": ip
+    }
+
+    ses = requests.Session()
+    signature, time = getSignature(PATH, "POST", API_SECRET)
+    res = ses.post(API_URL + PATH, headers={
+        "X-API-KEY": API_KEY,
+        "X-Signature": signature,
+        "X-Timestamp": str(time),
+        "X-CUSTOMER": CUSTOMER_ID,
+        'Content-Type': 'application/json'
+    }, data=json.dumps(body))
+    return res.json()
+
+
+def deleteIpExclusion(API_KEY: str, API_SECRET: str, CUSTOMER_ID: str, ids: list) -> list:
+    PATH = "/tool/ip-exclusions"
+
+    ses = requests.Session()
+    signature, time = getSignature(PATH, "DELETE", API_SECRET)
+    res = ses.delete(API_URL + PATH, headers={
+        "X-API-KEY": API_KEY,
+        "X-Signature": signature,
+        "X-Timestamp": str(time),
+        "X-CUSTOMER": CUSTOMER_ID,
+        'Content-Type': 'application/json'
+    }, params={"ids": ids})
+    return res.status_code
